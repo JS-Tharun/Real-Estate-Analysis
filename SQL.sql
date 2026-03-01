c2a11fa2a7b4242444ece72a19b69253568d0807
JS-Tharun <tharunjs06@gmail.com>
2026-03-01 10:55:01 +0530

    Visualization Update

--- SQL.sql
+++ SQL.sql
@@ -3,12 +3,6 @@ show databases;
 
 use real_estate;
 
-select * from agents;
-select * from buyers;
-select * from listings;
-select * from property_attributes;
-select * from sales;
-
 -- Property & Pricing Analysis
 -- Q1. What is the average listing price by city? (Currently in Use)
 select
