adbcb898d775f3aa5860805d95f7ff155967ae47
JS-Tharun <tharunjs06@gmail.com>
2026-03-03 23:42:28 +0530

    multipage

--- app/main.py
+++ app/main.py
@@ -1,21 +1,7 @@
 import streamlit as st
-from components import render_sidebar, render_dataframe, render_map, render_bar_chart, property_distribution_chart
 
-# Filter Sidebar
-render_sidebar()
+st.set_page_config(
+    page_title='Bricks View Analysis'
+)
 
-# Streamlit UI
-st.title("Real Estate Analysis Dashboard")
-
-# Dataframe
-render_dataframe()
-
-# Map
-render_map()
-
-# Bar chart
-render_bar_chart()
-
-# Pie Chart
-property_distribution_chart()
-
+st.title("Bricks View Analysis")
