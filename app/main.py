e44754ee67aeb55abab6860290d8e6054363037e
W-JST <tharunjs06@gmail.com>
2026-03-04 01:06:44 +0530

    Filter column created

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title='Bricks View Analysis'
+    page_title='BrickView Analysis'
 )
 
-st.title("Bricks View Analysis")
+st.title("BrickView Analysis")
--- app/pages/1_Properties.py
+++ app/pages/1_Properties.py
@@ -1,6 +1,37 @@
 import streamlit as st
+from components.properties import property_filter, city_filter, price_filter, agent_filter, from_l_date_filter, to_l_date_filter
+from utils import execute_query
 
 st.set_page_config(
-    page_title="Property Details",
+    page_title="Property Listings",
     layout='wide'
 )
+
+st.header("Property Listing")
+
+# Filter Bar
+with st.container():
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write("Col 1")
+
+    with col2:
+        st.write("Col 2")
+
+    with col3:
+        st.write("### Filter By")
+        city_filter()
+        property_filter()
+        price_filter()
+        agent_filter()
+        from_l_date_filter()
+        to_l_date_filter()
+
+        selected_property_status = st.selectbox(
+            "Property Status",
+            options=['All', 'Sold', 'Unsold']
+        )
+
+        st.write(f"{selected_property_status}")
+        
