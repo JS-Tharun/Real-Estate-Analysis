0079a856459153db3e753bc59c6805462c1656e1
W-JST <tharunjs06@gmail.com>
2026-03-03 12:16:48 +0530

    Created Bar Chart, Pie Chart

--- app/components.py
+++ app/components.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 from utils import execute_query
 from filters import filter, city_filter, price_filter, property_filter, property_filter, agent_filter, from_l_date_filter, to_l_date_filter
 
@@ -72,3 +73,46 @@ def render_map():
     latitude='Latitude',
     longitude='Longitude'
   )
+
+
+#Display Barchart
+
+def render_bar_chart():
+  query = """
+    select 
+    City,
+    count(*) as Number_of_Listings,
+    round(avg(price), 2) as Average_Price
+  from listings
+  group by city;
+  """
+  df = execute_query(query)
+  st.write("# Average Price by City")
+  st.bar_chart(
+    data=df,
+    x='City', x_label="City",
+    y='Average_Price', y_label="Price"
+  )
+  st.write("# Number of Properties by City")
+  st.bar_chart(
+    data=df,
+    x='City', x_label='City',
+    y='Number_of_Listings', y_label="Property Count"
+  )
+
+
+def render_pie_chart():
+  query = """
+    select
+    Property_Type,
+      round((100 * count(*) / (select count(*) from listings)), 2) as Percentage
+  from listings
+  group by Property_Type;
+  """
+  df = execute_query(query)
+  fig, ax = plt.subplots()
+  ax.pie(df['Percentage'], labels=df['Property_Type'], autopct='%.2f%%')
+  ax.axis('equal')
+
+  st.write("# Property Type Distribution")
+  st.pyplot(fig)
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from components import render_sidebar, render_dataframe, render_map
+from components import render_sidebar, render_dataframe, render_map, render_bar_chart, render_pie_chart
 
 # Filter Sidebar
 render_sidebar()
@@ -12,4 +12,10 @@ render_dataframe()
 
 # Map
 render_map()
+
+# Bar chart
+render_bar_chart()
+
+# Pie Chart
+render_pie_chart()
 
